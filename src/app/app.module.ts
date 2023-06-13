93b0b4ae44ed27bdb0eb877bcfeb50d89ba145e1
unknown <nguyendongminhtri29092014@gmail.com>
2023-06-13 16:50:58 +0700

    fe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-foot/nav-bar/nav-bar.component';
+import { FooterComponent } from './nav-foot/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import {MatCardModule} from "@angular/material/card";
+import { RegisterComponent } from './form-login/register/register.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './form-login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
